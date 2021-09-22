bcac80afa14f5cb4abee4bdcd34ced12deff4d67
Élie Deloumeau-Prigent <elie.deloumeau@fr.clara.net>
2021-09-22 17:03:02 +0200

    black

--- ansible-role-{{ cookiecutter.role_name }}/molecule/default/tests/test_default.py
+++ ansible-role-{{ cookiecutter.role_name }}/molecule/default/tests/test_default.py
@@ -5,8 +5,10 @@ import stat
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
 
 def test_shell(host):
-    command = host.run('sh --version')
+    command = host.run("sh --version")
     assert command.rc == 0
